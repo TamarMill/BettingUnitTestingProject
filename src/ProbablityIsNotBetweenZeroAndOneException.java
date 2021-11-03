ca79ed47e22893860c5f2edba6a61ffb59f79002
Tamar Miller <rmiller25@student.touro.edu>
2021-11-03 14:55:56 -0400

    Probability Exception Class

---
+++ src/ProbablityIsNotBetweenZeroAndOneException.java
@@ -0,0 +1,8 @@
+public class ProbablityIsNotBetweenZeroAndOneException extends Exception{
+    public ProbablityIsNotBetweenZeroAndOneException(){
+        super();
+    }
+    public ProbablityIsNotBetweenZeroAndOneException(String message){
+        super(message);
+    }
+}
