2c0fce1d8eeb7f95401334bef8f0ac4f628e5c84
Tamar Miller <rmiller25@student.touro.edu>
2021-11-03 12:45:57 -0400

    Betting class skeleton with dummy methods

---
+++ src/Betting.java
@@ -0,0 +1,27 @@
+public class Betting {
+    private int minBalance;
+    public Betting(IRandomValueGenerator random, int minBalance) {
+
+    }
+
+    public double getCurrentBalance() {
+         return 0;
+    }
+
+    public boolean canBet(double amnt) {
+        return true;
+    }
+
+    public void addMoney(double amnt) {
+
+    }
+
+    public double betOnANumber(double amnt, int min, int max, int selectedNumber) {
+        return 0;
+
+    }
+
+    public double betOnProbability(double amnt, double p) {
+        return 0;
+    }
+}
